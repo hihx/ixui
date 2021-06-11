b49bc81456d76efd9c78262f3530acbf2676e712
hx <215656007@qq.com>
2021-06-11 13:50:10 +0800

    auto

--- packages/loading/index.js
+++ packages/loading/index.js
@@ -24,4 +24,40 @@ Loading.service = (options) => {
   return instance && instance.thisVisible ? (instance = Object.assign(instance, options)) : Loading(options)
 }
 
+const loading_create = (el, binding) => {
+  if (!el.loading) {
+    el.loading = Loading({
+      target: binding.modifiers.fullscreen ? document.body : el,
+      model: binding.modifiers.model
+    })
+  }
+  if (!binding.modifiers.fullscreen) {
+    el.loading.$el.style.position = 'absolute'
+    let view = el.loading.$el.querySelector('.view')
+    view.style.marginTop = ((el.clientHeight) / 2 - view.clientHeight / 2) + 'px'
+  }
+}
+Vue.directive('loading', {
+  inserted: function (el, binding, vnode) {
+    if (!binding.modifiers.fullscreen) {
+      let style = window.getComputedStyle(el)
+      if (style && style.position == 'static') {
+        el.style.position = 'relative'
+      }
+    }
+    if (binding.value) {
+      loading_create(el, binding)
+    }
+  },
+  update: function (el, binding, vnode) {
+    if (binding.value) {
+      loading_create(el, binding)
+    }
+    else {
+      el.loading.close()
+      el.loading = null
+    }
+  }
+});
+
 export default Loading
--- packages/popover/index.js
+++ packages/popover/index.js
@@ -1,7 +1,15 @@
 import Popover from './Popover.vue'
+import ixLoading from "../loading";
 
 Popover.install = function(Vue) {
   Vue.component(Popover.name, Popover)
 }
+
+Vue.directive('popover', {
+  inserted: function (el, binding, vnode) {
+    let pop = vnode.context.$refs[binding.arg]
+    pop.bindEvent(el)
+  }
+});
 
 export default Popover
